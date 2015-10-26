3b1933caca66f1b1a25c41d6b723b00adc0cc02d
HuangXiyao <913456386@qq.com>
2015-10-26 22:43:44 +0800

    000

--- com/xml/Test.java
+++ com/xml/Test.java
@@ -3,5 +3,6 @@ package com.xml;
 public class Test {
 public static void main(String[] args) {
 	System.out.println("0000000000000000000000000000000000000000000000000000000000000000000000000000000000000");
+	System.out.println("nihao!");
 }
 }
