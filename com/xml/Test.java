6fc35f157d51d91fae26c99f02b7c196a1eec0be
HuangXiyao <913456386@qq.com>
2015-10-27 15:46:39 +0800

    modify master00000000000000

--- com/xml/Test.java
+++ com/xml/Test.java
@@ -4,6 +4,6 @@ public class Test {
 public static void main(String[] args) {
 	System.out.println("0000000000000000000000000000000000000000000000000000000000000000000000000000000000000");
 	System.out.println("nihao!");
-	System.out.println("nihao55555555555555555555!");
+	System.out.println("nihao5555555000000000000=================5555555555555!");
 }
 }
